87b3a5de9533ac00e24eadbf26e4888bbf07fe52
Manash Ranjan Rautray <manash.rautray@mindfiresolutions.com>
2023-02-15 16:38:46 +0530

    comment

---
+++ comment.py
@@ -0,0 +1,7 @@
+# Comments starts with a #, and Python will ignore them
+"""
+This is a comment
+written in
+more than just one line but since its not assigned to any variable,this will be ignored.
+"""
+
